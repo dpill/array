17dcc1cb9a77609e7e4f22a5b43436c1ffbc494b
dpill <a20141105059@mac05.local>
2015-12-16 19:23:13 +0800

    数组越界

--- array/main.cpp
+++ array/main.cpp
@@ -10,6 +10,10 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a[4],b[4];
+    a[4]=123;
+    b[4]=456;
+    printf("a=%x &a[4]=%x b=%x &b[4]=%x\n",a,&a[4],b,&b[4]);
+    printf("a[0]=%d b[0]=%d a[4]=%d b[4]=%d\n",a[0],b[0],a[4],b[4]);
     return 0;
 }
